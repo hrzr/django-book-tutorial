cfd651e1f168435a1a26634e4a260bc4d634c0ea
Mikhail Gaivoronskii <m.gaivoronskii@astondevs.ru>
2022-12-02 12:38:23 +0300

    Configured project for deployment

--- test-project/config/settings.py
+++ test-project/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l9su!2k8#n)m@qpai39f(!19=l6t1c7c)!2s9w4m0_buh0lelh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
