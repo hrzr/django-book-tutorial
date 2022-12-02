9f437931c2e6894d96823d7dd76aca0b265c2a55
Mikhail Gaivoronskii <m.gaivoronskii@astondevs.ru>
2022-12-02 10:56:38 +0300

    Added templates, created tests, created two views

--- test-project/config/urls.py
+++ test-project/config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include('pages.urls')),
+    path('pages/', include('pages.urls')),
 ]
--- test-project/pages/views.py
+++ test-project/pages/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def home_page_view(request):
-    return HttpResponse('<h1>hello, world!</h1>')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
