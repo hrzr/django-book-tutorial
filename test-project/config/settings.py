4020534702a67c28bfc4f613bc9f7917d2877645
Mikhail Gaivoronskii <m.gaivoronskii@astondevs.ru>
2022-12-05 10:31:59 +0300

    Refactored STATIC_ROOT one more time

--- test-project/config/settings.py
+++ test-project/config/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/var/www/mgaivoronskii.pythonanywhere.com/static/'
+STATIC_ROOT = '/home/mgaivoronskii/django-book-tutorial/test-project/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
