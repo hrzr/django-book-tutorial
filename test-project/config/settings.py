c54e4577396ae10f4c47f257da30caeaeefb92c6
Mikhail Gaivoronskii <m.gaivoronskii@astondevs.ru>
2022-12-05 10:40:29 +0300

    Continued to experiment with STATIC_ROOT 1

--- test-project/config/settings.py
+++ test-project/config/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = '/var/www/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/mgaivoronskii/django-book-tutorial/test-project/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
